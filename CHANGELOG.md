550671cb555d993dd39e3cda0c2967f7d7ab6b96
Hermann Mayer <hermann.mayer92@gmail.com>
2020-07-14 14:27:00 +0200

    Updated the CI setup and improved the on_muc_filter_message hook.
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## next
+
+* Added a check for the affiliated users meta data of a packet
+* Fixed the CI setup
+
 ## 0.2.0
 
 * Added the (sender/receiver(s)) vCards on the XML event messages
--- include/mod_mam2sidekiq.hrl
+++ include/mod_mam2sidekiq.hrl
@@ -1,4 +1,4 @@
--define(MODULE_VERSION, <<"0.2.0-476">>).
+-define(MODULE_VERSION, <<"0.2.0-477">>).
 -define(NS_MAM_SIDEKIQ, <<"xmpp:mam:hausgold:sidekiq">>).
 
 %% A macro to convert a record to tuple{[tuples]} for jiffy (JSON) encoding
